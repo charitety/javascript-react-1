0febb15a9fe636b46d9ba7d87265b40f52f547bb
Caridad RoblesCarvajal <croblescarvajal@xpanxion.com>
2023-05-17 17:59:47 -0400

    complete example2

---
+++ src/Student1/Ex2/Tags.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Tags = () => {
+    return (
+        <div id= "Tags">
+            <p>Home | FAQ | Support | Account</p> 
+        </div>
+      
+    )
+}
